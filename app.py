edaa9ca45efa302ed48c7cea8ca1b679506e8892
Daniel <dwolela@gmail.com>
2021-11-19 22:55:35 +0400

    Added javascript chart.js to show nutrition values

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import json
 import requests
 from flask.wrappers import Response
 from werkzeug.wrappers import response
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,8 +14,14 @@ def index():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     key = request.form['key']
-    response = requests.get('https://api.nal.usda.gov/fdc/v1/foods/search?query='+key+'&pageSize=2&api_key=Hal64gsGkYVlg6bhIAn5zQoLR6GqbuSzWa5LsJLj')
-    result = json.loads(response.text)
-    data_dict = {data['nutrientName'] : data['value'] for data in result['foods'][0]['foodNutrients']}
-    data = dict(sorted(data_dict.items(), key = lambda x: x[1])[-10:])
-    return render_template('pages/search.html', resultdata=data, item=key)
+    try:
+        response = requests.get('https://api.nal.usda.gov/fdc/v1/foods/search?query='+key+'&pageSize=2&api_key=Hal64gsGkYVlg6bhIAn5zQoLR6GqbuSzWa5LsJLj')
+        result = json.loads(response.text)
+        data_dict = {data['nutrientName'] : data['value'] for data in result['foods'][0]['foodNutrients']}
+        data = dict(sorted(data_dict.items(), key = lambda x: x[1])[-10:])
+        label = list(data.keys())
+        value = list(data.values())
+    except:
+        data = {}
+
+    return render_template('pages/search.html', resultdata=data, item=key, label=label, value=value)
