e56dda4d54e3f361bd98182833879a54f9a9551e
Daniel <dwolela@gmail.com>
2021-11-21 07:43:08 +0400

    Pull nutrients summary from wikipedia, charting, loading animation

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, app, render_template, request, redirect, url_for, flash, jsonify
 import json
 import requests
+import wikipedia
 from flask.wrappers import Response
 from werkzeug.wrappers import response
-
-
+import re
+# Create the application instance
 app = Flask(__name__)
+# Create secret key for session
+app.secret_key = "super secret key"
 
+# Create a URL route in our application for "/"
 @app.route('/')
 def index():
-    return render_template('pages/search.html', resultdata={}, item ="" )
+    return render_template('pages/home.html', resultdata={}, item ="" )
 
+# Create a URL route in our application for "/search"
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     key = request.form['key']
+    wiki_result = wikipedia.summary(f'{key} in diet')
+    print(wiki_result)
     try:
         response = requests.get('https://api.nal.usda.gov/fdc/v1/foods/search?query='+key+'&pageSize=2&api_key=Hal64gsGkYVlg6bhIAn5zQoLR6GqbuSzWa5LsJLj')
         result = json.loads(response.text)
@@ -23,5 +30,24 @@ def search():
         value = list(data.values())
     except:
         data = {}
-
+        label = []
+        value = []
+        flash('No resulet were found for keyword='+key)
     return render_template('pages/search.html', resultdata=data, item=key, label=label, value=value)
+
+@app.route('/lookup/<keyword>')
+def lookup(keyword):
+    if keyword == 'None':
+        return render_template('pages/search.html', resultdata={}, item ="" )
+    else:
+        if ',' not in keyword:
+            keyword = ' '.join(re.findall('[A-Za-z]+', keyword))
+        nutrient = keyword.split(',')[0]
+        search_key = f'{nutrient} in diet'
+        wiki_result = wikipedia.summary(search_key, sentences=5)
+        print(wiki_result)
+     
+        return jsonify(data=wiki_result) 
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,27 @@
-window.parseISOString = function parseISOString(s) {
-  var b = s.split(/\D+/);
-  return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
-};
+// window.parseISOString = function parseISOString(s) {
+//   var b = s.split(/\D+/);
+//   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
+// };
+$(document).ready(function() {
+  $('#nutrients').click(function(e) {
+    e.preventDefault();
+    event_target = e.target
+    // target = $(event_target).prop('tagName');
+    selected_value = event_target.text;
+    $(".loader").css("display", "block"); 
+    $.ajax({
+      url: '/lookup/'+selected_value,
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        // alert(data.data)
+        $("#info-title").html(selected_value.split(',')[0])
+        $(".loader").css('display', 'none');
+        $("#info-text").html(data.data)
+        $("#info-box").css("display", "block");
+
+      }
+    });
+
+  });
+});
